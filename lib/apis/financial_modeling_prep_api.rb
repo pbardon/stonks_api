125aa1bdcbc868c6080bc8ae3923a6dd34850981
Peter Bardon <ptrbardon@gmail.com>
2021-06-24 13:34:20 -0700

    add redis cache to track number of external api requests and add scheduler to reset count

--- lib/apis/financial_modeling_prep_api.rb
+++ lib/apis/financial_modeling_prep_api.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
+
 require_relative 'api'
 # Create this API client in the Apis module
 module Apis
   # Inherit the common methods from the Api class
   class FinancialModelingPrepApi < Apis::Api
     attr_accessor :ticker, :url
-    attr_accessor :allowed_daily_requests
 
     URL_BASE = 'https://financialmodelingprep.com/api/v3/historical-price-full'
 
@@ -13,54 +13,46 @@ module Apis
       @api_key = api_key
       @ticker = ticker
       @request_count = 0
-      @allowed_daily_requests = 100
+      @request_count_key = 'fmp_api_available_daily_requests'
+      @cache = RedisCache.new
+      FinancialModelingPrepApi.reset_count unless @cache.get(@request_count_key)
       super("#{URL_BASE}/#{@ticker}?apikey=#{@api_key}")
-    end
-
-    def render_mock_api
-      json_file = File.open("#{Rails.root}/spec/data/aapl_historical.json")
-       # turn the response into a JSON object
-      return JSON.parse(json_file.read)
-    end
-
-    def update_ticker(ticker)
-      @ticker = ticker
-    end
-
-    def set_url(url)
-      @url = url
     end
 
     def find
       # turn the response into a JSON object
-      increment_request_count
       return false if daily_request_limit_exceeded?
 
-      result = render_mock_api
-      result['symbol'] = ticker
-      result
+      decrement_request_count
+      # query the api
+      query
     end
 
     def daily_request_limit_exceeded?
-      @request_count > @allowed_daily_requests
+      Integer(@cache.get(@request_count_key)) < 1
     end
-  
-    def increment_request_count
-      @request_count += 1
-    end
-  
-    def reset_count
-      @request_count = 0
+
+    def decrement_request_count
+      count = Integer(@cache.get(@request_count_key))
+      @cache.set(@request_count_key, count - 1)
     end
 
     def api_key_set?
-      return !!@api_key
+      !!@api_key
     end
 
     # Static methods
+    def self.reset_count(count = 5)
+      RedisCache.new.set(
+        'fmp_api_available_daily_requests',
+        count
+      )
+    end
+
     def self.parse_price(raw_price_data)
       {
         open: raw_price_data['open'],
+        date: raw_price_data['date'],
         close: raw_price_data['close'],
         high: raw_price_data['high'],
         low: raw_price_data['low'],
--- spec/lib/financial_modeling_prep_api_spec.rb
+++ spec/lib/financial_modeling_prep_api_spec.rb
@@ -1,39 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe Apis::FinancialModelingPrepApi do
-  describe('initialization') do
-    it('can create the api client') do
+  describe('#initialize') do
+    before(:all) do
       mock_api_key = 'foobar'
       mock_ticker = 'DDD'
-      api_client = Apis::FinancialModelingPrepApi.new(mock_ticker, mock_api_key)
-      expect(api_client.ticker).to_not be_nil
-      expect(api_client.url).to_not be_nil
-      expect(api_client.api_key_set?).to be true
+      @api_client = Apis::FinancialModelingPrepApi.new(mock_ticker, mock_api_key)
+    end
+    it('can create the api client') do
+      expect(@api_client.ticker).to_not be_nil
+      expect(@api_client.url).to_not be_nil
+      expect(@api_client.api_key_set?).to be true
+    end
+
+    it('formats the url correctly') do
+      expect(@api_client.url).to eq(
+        'https://financialmodelingprep.com/api/v3/historical-price-full/DDD?apikey=foobar'
+      )
     end
   end
 
-  describe('can be created as a singleton') do
-    # todo: figure out singleton pattern for rails
+  describe('#find') do
+    before(:each) do
+      # mock out Apis::Api query method to return the correct output
+      Apis::Api.subclasses.each do |klass|
+        allow_any_instance_of(klass).to receive(:query).and_return({})
+      end
+
+      mock_api_key = 'foobar'
+      mock_ticker = 'DDD'
+      @api_client = Apis::FinancialModelingPrepApi.new(mock_ticker, mock_api_key)
+    end
+
+    it('can query the api') do
+      expect(@api_client.find).to eq({})
+    end
+
+    it 'should prevent requests over the daily allowed maximum' do
+      5.times do
+        expect(@api_client.find).to eq({})
+      end
+      expect(@api_client.find).to be(false)
+    end
+
+    it 'should allow additional requests once the count is reset' do
+      Apis::FinancialModelingPrepApi.reset_count(0)
+      expect(@api_client.find).to be(false)
+      Apis::FinancialModelingPrepApi.reset_count
+      expect(@api_client.find).to eq({})
+    end
   end
 
-  # describe('querying the api') do
-  #   mock_api_key = 'foobar'
-  #   mock_ticker = 'DDD'
-  #   api_client = Apis::FinancialModelingPrepApi.new(mock_ticker, mock_api_key)
-  #   # mock out Apis::Api get method to return the correct output
-  #   result = api_client.find
-  # end
+  describe('#self.parse_price') do
+    it 'should parse a single price entry as an object' do
+      mock_raw_price_data = JSON.parse('
+        {
+        "date" : "2021-06-01",
+        "open" : 125.08,
+        "high" : 125.35,
+        "low" : 123.95,
+        "close" : 124.28,
+        "adjClose" : 124.28,
+        "volume" : 6.3890796E7,
+        "unadjustedVolume" : 6.3890796E7,
+        "change" : -0.8,
+        "changePercent" : -0.64,
+        "vwap" : 124.52667,
+        "label" : "June 01, 21",
+        "changeOverTime" : -0.0064
+        }
+      ')
 
-  # describe('limiting the number of daily requests') do
-  #   it 'should prevent requests over the daily allowed maximum' do
-  #     mock_api_key = 'foobar'
-  #     mock_ticker = 'DDD'
-  #     api_client = Apis::FinancialModelingPrepApi.new(mock_ticker, mock_api_key)
-  #     api_client.allowed_daily_requests = 5
-  #     5.times do 
-  #       expect(api_client.find).to be_nil
-  #     end
-  #     expect(api_client.find).to be(false)
-  #   end
-  # end
+      Apis::FinancialModelingPrepApi.parse_price(mock_raw_price_data)
+    end
+  end
 end
