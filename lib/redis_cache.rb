bfa950d632e33b2ee63b7e69bbb6685081b0ef61
Peter Bardon <ptrbardon@gmail.com>
2021-06-25 02:01:11 -0700

    update redis url and add ssl configuration setting

--- lib/redis_cache.rb
+++ lib/redis_cache.rb
@@ -1,6 +1,8 @@
 class RedisCache
   def self.new
     Redis.new(Rails.application.config_for(:redis)).flushdb if Rails.env == 'test'
-    Redis.new(Rails.application.config_for(:redis))
+    Redis.new(Rails.application
+      .config_for(:redis)
+      .merge({ ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }))
   end
 end
